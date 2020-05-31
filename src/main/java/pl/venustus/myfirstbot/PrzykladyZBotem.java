099fe9db61805727ca85f547346138e6cbc088f6
venustuspl <venustus.pl@gmail.com>
2020-05-31 22:24:36 +0200

    0.0.2 "Jestem Botem"

--- src/main/java/pl/venustus/myfirstbot/PrzykladyZBotem.java
+++ src/main/java/pl/venustus/myfirstbot/PrzykladyZBotem.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pl.venustus.myfirstbot;
 
 import java.awt.AWTException;
@@ -12,11 +7,6 @@ import java.awt.event.KeyEvent;
 import java.util.Random;
 
 
-
-/**
- *
- * @author konra
- */
 public class PrzykladyZBotem {
     public static void main(String[] args) throws IOException, AWTException {
         String nazwa = "notepad.exe";
@@ -24,8 +14,7 @@ public class PrzykladyZBotem {
         run.exec(nazwa);
         Robot robot = new Robot();
         robot.setAutoDelay(500);
-        //tu znajduje się komentarz
-        robot.keyPress(KeyEvent.VK_J); 
+        robot.keyPress(KeyEvent.VK_J);
         robot.keyPress(KeyEvent.VK_E); 
         robot.keyPress(KeyEvent.VK_S); 
         robot.keyPress(KeyEvent.VK_T); 
@@ -37,9 +26,6 @@ public class PrzykladyZBotem {
         robot.keyPress(KeyEvent.VK_T); 
         robot.keyPress(KeyEvent.VK_E); 
         robot.keyPress(KeyEvent.VK_M); 
-        /* Tutaj także
-        znajduje się komentarz, jednak
-        zawierający kilka linijek */
         Random r = new Random();
         for (int i=0; i<10; i++){
             robot.mouseMove(r.nextInt(1200),r.nextInt(800));
