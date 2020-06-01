df07abb2495cc35cb0bde153527d99435f4488d3
venustuspl <venustus.pl@gmail.com>
2020-06-01 22:04:24 +0200

    0.0.3

--- src/main/java/pl/venustus/myfirstbot/Mybot.java
+++ src/main/java/pl/venustus/myfirstbot/Mybot.java
@@ -1,6 +1,8 @@
 package pl.venustus.myfirstbot;
 
 import java.awt.AWTException;
+import java.awt.event.InputEvent;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.awt.Robot; 
 import java.awt.event.KeyEvent;
@@ -9,11 +11,12 @@ import java.util.Random;
 
 public class Mybot {
     public static void main(String[] args) throws IOException, AWTException {
-        String nazwa = "notepad.exe";
+        Robot robot = new Robot();
+
+        /*String nazwa = "notepad.exe";
         Runtime run = Runtime.getRuntime();
         run.exec(nazwa);
-        Robot robot = new Robot();
-        robot.setAutoDelay(500);
+        robot.setAutoDelay(50);
         robot.keyPress(KeyEvent.VK_J);
         robot.keyPress(KeyEvent.VK_E); 
         robot.keyPress(KeyEvent.VK_S); 
@@ -25,10 +28,13 @@ public class Mybot {
         robot.keyPress(KeyEvent.VK_O); 
         robot.keyPress(KeyEvent.VK_T); 
         robot.keyPress(KeyEvent.VK_E); 
-        robot.keyPress(KeyEvent.VK_M); 
+        robot.keyPress(KeyEvent.VK_M); */
         Random r = new Random();
         for (int i=0; i<10; i++){
             robot.mouseMove(r.nextInt(1200),r.nextInt(800));
+            robot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
+            robot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
+            robot.setAutoDelay(900);
         }
     }
 }
