600b11f6370c2df90bd2b51ae43591e4ec2e8037
venustuspl <venustus.pl@gmail.com>
2020-06-02 22:47:04 +0200

    0.0.4 Added loger

--- src/main/java/pl/venustus/myfirstbot/Mybot.java
+++ src/main/java/pl/venustus/myfirstbot/Mybot.java
@@ -8,9 +8,12 @@ import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.time.LocalDateTime;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Mybot {
+    private static final Logger LOGGER = Logger.getLogger( Mybot.class.getName() );
     public static void main(String[] args) throws IOException, AWTException {
         Robot robot = new Robot();
 
@@ -31,12 +34,14 @@ public class Mybot {
         robot.keyPress(KeyEvent.VK_E); 
         robot.keyPress(KeyEvent.VK_M); */
         Random r = new Random();
-        for (int i=0; i<100; i++){
+        for (int i=0; i<10; i++){
             robot.mouseMove(r.nextInt(1200),r.nextInt(800));
             robot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
             robot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
             robot.setAutoDelay(2000);
             System.out.println(i + " " + LocalDateTime.now());
+            LOGGER.log( Level.FINE, "processing {0} entries in loop", i);
+            LOGGER.log(Level.INFO , " " + i);
         }
     }
 }
